9febf2af280b08893ae718222691efd80cafd47e
xuanyupan <982407098@qq.com>
2018-12-19 22:05:44 +0800

    在写一次反转链表

---
+++ Algorithms/Array_LinkedList/LeetCode_206_Again.java
@@ -0,0 +1,28 @@
+package Algorithms.Array_LinkedList;
+
+/**
+ * @Author : xuanyupan
+ * @Time : 2018/12/19 20:44
+ * @Desc : 再写一遍反转链表
+ */
+public class LeetCode_206_Again {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode new_head = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = new_head;
+            new_head = head;
+            head = temp;
+        }
+        return new_head;
+    }
+}
---
+++ Algorithms/Array_LinkedList/LeetCode_92.java
@@ -0,0 +1,9 @@
+package Algorithms.Array_LinkedList;
+
+/**
+ * @Author : xuanyupan
+ * @Time : 2018/12/19 20:55
+ * @Desc : TODO
+ */
+public class LeetCode_92 {
+}
